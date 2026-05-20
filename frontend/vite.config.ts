951eed494747807261250e681ad6469da8f8a5be
“Mayank <mayanksrivastava11111@gmail.com>
2026-05-20 15:22:09 +0530

    start command

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    allowedHosts: [
+      'smartleads-frontend-v1ho.onrender.com'
+    ]
+  }
 })
